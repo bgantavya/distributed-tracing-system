f4a61222c75814985579412f63881ab399b6d310
Gantavya Bansal <gantavyaoo@gmail.com>
2026-02-18 11:31:52 +0530

    minimized --test-success

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,3 +1,5 @@
+import { UserProps } from "./types.js";
+
 export const Paths = {
     user: {
         base: () => '/user/:id',
@@ -11,12 +13,18 @@ export const Paths = {
     },
 };
 
-export enum RoleTypes {
+export enum Roles {
 	Member = "member",
 	Admin = "admin",
 }
 
-export enum ServeTypes {
+export enum DeployT {
     Prod = "production",
     Dev = "development",
 }
+    
+export const users: UserProps[] = [
+    { id: 0, name: "tj", email: "tj@vision-media.ca", role: Roles.Member },
+    { id: 1, name: "ciaran", email: "ciaranj@gmail.com", role: Roles.Member },
+    { id: 2, name: "aaron", email: "aaron.heckmann+github@gmail.com", role: Roles.Admin },
+];
--- src/utils/users.ts
+++
@@ -1,8 +0,0 @@
-import { RoleTypes } from "./constant";
-import type { UserProps } from "./types";
-
-export const users: UserProps[] = [
-    { id: 0, name: "tj", email: "tj@vision-media.ca", role: RoleTypes.Member },
-    { id: 1, name: "ciaran", email: "ciaranj@gmail.com", role: RoleTypes.Member },
-    { id: 2, name: "aaron", email: "aaron.heckmann+github@gmail.com", role: RoleTypes.Admin },
-];
