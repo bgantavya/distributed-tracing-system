8395945ee94f512a4b70a6c2761898bceea0627a
Gantavya Bansal <gantavyaoo@gmail.com>
2026-05-02 14:51:49 +0530

    added:cors

--- index.ts
+++ index.ts
@@ -8,6 +8,18 @@ config();
 const app = express();
 const PORT = Number(process.env.PORT) || 1234;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.use(traceRequest);
 app.get('/', (req, res) => res.send('Server is Working!'));
 
